432d3714cef186af661b42b04d097e675b761190
Kids-tools <143388748+Kids-tools@users.noreply.github.com>
2023-11-05 12:47:39 +0600

    Add files via upload

---
+++ DX-ABAL.py
@@ -0,0 +1,14 @@
+import os,time,random
+os.system('clear') 
+print(' [>] BY : SHISHIR AHMED Z')
+
+print(' [>] FIRST RUN TOOLS && COPY APPROVL KEY ')
+print(' [>] Username >> DX \n [>] Password >> POWER ')
+print(15*'\033[1;92m<>\033[1;97m')
+os.system('rm -rf android8.txt')
+int = input(' [?] INPUT YOUR KEY >> ')
+open('android8.txt','a').write(int)
+print(' [>] Please Wait...')
+print(' [>] Bypass Done 😴')
+
+os.system('python DX_2.0_Fucked.py')
